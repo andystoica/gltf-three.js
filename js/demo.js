d91d5cd69ba0233ad487bf4939c3f839fcf9d2e1
Andy Stoica <andy.stoica@me.com>
2020-05-29 22:40:52 +0100

    Fixed description typos

--- js/demo.js
+++ js/demo.js
@@ -1,14 +1,15 @@
 /**
  * Simple demonstration for rendering a GLTF 3D Object using the Three.js library.
- * https://threejs.org/
+ * https://threejs.org
  *
- * 3D model courtesy of jeff1234678@Sketchfab
+ * 3D model courtesy of jeff1234678
  * https://sketchfab.com/3d-models/coronavirus-6d90f835a3bb45b0b46e44ceafd07524
  *
- * This demonstrations initialises a 3D scene as a canvas element attached to the body element.
- * The 3d model is impoted using the GLTFLoader module and is rendered using 3 lights
- * and a perspective camera. The model is then pun on an animation loop, gently rotating it around
- * the X an Y axis. Camera controls are achieved with mouse interactions the OrbitControls module.
+ * This demonstration initialises a 3D scene as a canvas element attached to the body element.
+ * The 3D model is imported using the GLTFLoader module and is rendered using 3 lights and
+ * a perspective camera. The model is then pun on an animation loop, gently rotating it
+ * around the X and Y axis. Camera controls are achieved with mouse interactions
+ * the OrbitControls module.
  */
 
  
